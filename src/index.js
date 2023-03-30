ad9ad663cde91019e1436bde89641972f75b875d
lonebots <jishnusurajila@gmail.com>
2023-03-31 00:04:27 +0530

    fix: index file updated

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {SettingsContextProvider} from './context/SettingsContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <SettingsContextProvider>
+        <App />
+    </SettingsContextProvider>
+);
 
